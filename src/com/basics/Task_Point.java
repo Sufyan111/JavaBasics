eedc8448ab2b1119f4310efb9ff2b3eb22e77237
Sufyan111 <syedaleem1742@gmail.com>
2021-10-11 19:25:55 +0530

    Task 1 & 2

---
+++ src/com/basics/Task_Point.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class Task_Point {
+	
+	private int length, width, height;
+	public void showDimensions() {
+		System.out.println("Box Dimensions: "+length+" "+width+" "+height);
+	}
+	public static void main(String[] args) {
+		Point2D Point2D Point2D Point2D Point2D 
+	}	
+}
---
+++ src/com/basics/Task_point2.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class Task_point2 {
+	private int x,y;
+
+	public String getDetails(int a,int b)
+	{
+		x=a;
+		y=b;
+		return "Point ("+x+","+y+")";
+	}
+}
